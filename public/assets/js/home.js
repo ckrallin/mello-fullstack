77f21ab31a260d3c61b9e12dd9adbcb9b60760fa
Conner Krall <conner.krall@gmail.com>
2020-01-28 22:37:54 +0000

    added inputs for email/password, display message when email or password null, added ajax request for email/password

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,6 +1,9 @@
 const $setLogin = $('#login');
 const $setSignUp = $('#signup');
 const $submitButton = $('#submit');
+const $emailInput = $('#email');
+const $passwordInput = $('#password');
+const $message = $('#message');
 
 let authSetting = 'login';
 
@@ -17,5 +20,42 @@ function setAuth(setting) {   //function to toggle login/signup buttons
   }
 }
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
+
+  if (!email || !password) {
+    displayMessage('Email and password fields cannot be blank.', 'danger'); //returns error if either field is blank
+    return;
+  }
+
+  $emailInput.val('');
+  $passwordInput.val('');
+
+  authenticateUser(email, password);
+}
+
+function displayMessage(message, type) {
+  $message.text(message).attr('class', type);
+}
+
+function authenticateUser(email, password) {
+  $.ajax({
+    url: '/' + authSetting, //directs the path based on the authSetting
+    data: {
+      user: {
+        email,
+        password
+      }
+    },
+    method: 'POST'
+  }).then(function(data) {
+    console.log(data);
+  });
+}
+
 $setLogin.on('click', setAuth.bind(null, 'login'));
 $setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handleFormSubmit)
