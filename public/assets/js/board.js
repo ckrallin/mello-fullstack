448fcb8225539216d39c02b557e9a21b41c01197
Conner Krall <conner.krall@gmail.com>
2020-02-05 18:49:48 +0000

    add functionality to remove a contributor

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -35,7 +35,9 @@ function getBoard(id) {
     renderBoard();
   })
   .catch(function(err) {
-    location.replace('/boards');  ///redirect to boards page if there is an error
+    if (err.statusText === 'Unauthorized') {
+      location.replace('/boards');  ///redirect to boards page if there is an error
+    }
   });
 }
 
@@ -116,7 +118,14 @@ function renderBoard() {
 
 function renderContributors() {
   let $contributorListItems = board.users.map(function(user) {
-    let $contributorListItem = $('<li>').text(user.email);
+    let $contributorListItem = $('<li>');
+    let $contributorSpan = $('<span>').text(user.email);
+    let $contributorDeleteButton = $('<button class="danger">Remove</button>')
+      .data(user)
+      .on('click', handleContributorDelete);
+
+    $contributorListItem.append($contributorSpan, $contributorDeleteButton);
+
     return $contributorListItem;
   });
 
@@ -399,6 +408,22 @@ function openContributorModal() {
   displayMessage('');
 
   MicroModal.show('contribute');
+}
+
+function handleContributorDelete(event) {
+  let { id, email } = $(event.target).data();
+
+  $.ajax({
+    url: '/api/user_boards',
+    method: 'DELETE',
+    data : {
+      user_id: id,
+      board_id: board.id
+    }
+  }).then(function(){
+    init();
+    displayMessage(`Successfully removed user: ${email}`, 'success');
+  });
 }
 
 function displayMessage(msg, type = 'hidden') {
